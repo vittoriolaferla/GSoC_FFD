53f5fe9ce3c4b9bc434be54e1cf1a4f30d65b0db
vittoriolaferla <vittorio.laferla@mail.polimi.it>
2025-02-15 17:02:53 +0100

    Adding permission to acces the folder simualtion results

--- shellScript.sh
+++ shellScript.sh
@@ -17,6 +17,10 @@ start_time=$(date +%s)
 # Change to the submission directory (if needed)
 cd "$SLURM_SUBMIT_DIR"
 
+
+sudo mkdir /cluster/home/vlaferla/simulation_output          # if it doesn't exist already
+sudo chmod 777 /cluster/home/vlaferla/simulation_output 
+
 # Ensure the executable has proper permissions
 chmod +x /cluster/home/vlaferla/GSoC_FFD/bin/Release/net9.0/linux-x64/publish/GSoC_FFD
 
